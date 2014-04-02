f8686f8ce4b6ea67fb213fbb1ccf2fcf37800ac5
n24chris05 <ChrisiMac@Chriss-iMac-5.local>
2014-04-02 23:16:00 +0800

    instance variable

--- RandomItems/BNRItem.m
+++ RandomItems/BNRItem.m
@@ -1,13 +1,38 @@
-//
-//  BNRItem.m
-//  RandomItems
-//
-//  Created by Chris Arquelada on 4/2/14.
-//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
-//
 
 #import "BNRItem.h"
 
 @implementation BNRItem
+
+-(void)setItemName:(NSString *)str
+{
+    _itemName = str;
+}
+-(NSString *)itemName
+{
+    return _itemName;
+    
+}
+-(void)setSerialNumber:(NSString *)str
+{
+    _serialNumber = str;
+    
+}
+-(NSString *)serialNumber
+{
+    return _serialNumber;
+}
+-(void)setValueInDollars:(int)v
+{
+    _valueInDollars = v;
+}
+-(int)valueInDollars
+{
+    return _valueInDollars;
+}
+-(NSDate *)dateCreated
+{
+    return _dateCreated;
+}
+
 
 @end
--- RandomItems/main.m
+++ RandomItems/main.m
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BNRItem.h"
 
 int main(int argc, const char * argv[])
 {
@@ -30,6 +31,10 @@ int main(int argc, const char * argv[])
             //Log the description of item
             NSLog (@"%@",item);
         }
+        
+        BNRItem *item = [[BNRItem alloc]init];
+        NSLog (@" %@ %@ %@  %d",[item itemName],[item dateCreated],
+               [item serialNumber],[item valueInDollars]);
         
         
         //Destroy the mutable array object
